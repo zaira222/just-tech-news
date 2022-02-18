5a490dc6aebf1a33edf8a574163530add6d7fa4e
ZP <zairaperez01@gmail.com>
2022-02-17 23:07:36 -0600

    fixed post.js

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,6 @@ let sequelize;
 if(process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-// create connection to our db
 sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
   dialect: 'mysql',
